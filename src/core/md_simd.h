937e0722bdd6e6c1ee3c21ee4a6f5a428ffa4d64
scanberg <scanberg@gmail.com>
2021-11-24 21:33:07 +0100

    simd updates

--- src/core/md_simd.h
+++ src/core/md_simd.h
@@ -35,6 +35,7 @@
     #define md_simd_maxf    md_simd_max_f256
     #define md_simd_signf   md_simd_sign_f256
     #define md_simd_signcopyf   md_simd_signcopy_f256
+    #define md_simd_blendf  md_simd_blend_f256
 
     #define md_simd_horizontal_minf md_simd_horizontal_min_f256
     #define md_simd_horizontal_maxf md_simd_horizontal_max_f256
@@ -84,6 +85,8 @@
     #define md_simd_maxf    md_simd_max_f128
     #define md_simd_signf   md_simd_sign_f128
     #define md_simd_signcopyf   md_simd_signcopy_f128
+    #define md_simd_blendf  md_simd_blend_f128
+
 
     #define md_simd_horizontal_minf md_simd_horizontal_min_f128
     #define md_simd_horizontal_maxf md_simd_horizontal_max_f128
@@ -151,6 +154,10 @@ static inline __m128 md_simd_sign_f128(__m128 x) {
 static inline __m128 md_simd_copysign_f128(__m128 mag, __m128 sign) {
     __m128 const mask = _mm_set1_ps(-0.0f);
     return _mm_or_ps(_mm_and_ps(mask, mag), _mm_andnot_ps(mask, sign));
+}
+
+static inline __m128 md_simd_blend_f128(__m128 a, __m128 b, __m128 mask) {
+    return _mm_blendv_ps(a, b, mask);
 }
 
 static inline __m128 md_simd_min_f128(__m128 a, __m128 b) { return _mm_min_ps(a, b); }
@@ -267,6 +274,10 @@ static inline __m256 md_simd_sign_f256(__m256 x) {
 static inline __m256 md_simd_copysign_f256(__m256 mag, __m256 sign) {
     __m256 const mask = _mm256_set1_ps(-0.0f);
     return _mm256_or_ps(_mm256_and_ps(mask, mag), _mm256_andnot_ps(mask, sign));
+}
+
+static inline __m256 md_simd_blend_f256(__m256 a, __m256 b, __m256 mask) {
+    return _mm256_blendv_ps(a, b, mask);
 }
 
 static inline __m256 md_simd_min_f256(__m256 a, __m256 b) { return _mm256_min_ps(a, b); }
