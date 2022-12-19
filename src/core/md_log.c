8b49aefbe954fa799a8be5aa00c5a156a5b8a9fa
scanberg <scanberg@gmail.com>
2022-12-19 14:47:02 +0100

    log: added explicit values to defines to reduce windows header include stuff, Don't know if it is required....

--- src/core/md_log.c
+++ src/core/md_log.c
@@ -9,11 +9,11 @@
 #endif
 
 #ifndef VC_EXTRALEAN
-#define VC_EXTRALEAN
+#define VC_EXTRALEAN 1
 #endif
 
 #ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN 1
 #endif
 
 #include <Windows.h>
