dad5886a05a2fd7d33071409a07309f4eb6fc35a
scanberg <scanberg@gmail.com>
2023-01-22 23:13:26 +0100

    string_builder: Added some ergonomics

--- src/core/md_str_builder.h
+++ src/core/md_str_builder.h
@@ -9,6 +9,13 @@ struct md_allocator_i;
 typedef struct md_strb_t {
     char* buf;
     struct md_allocator_i* alloc;
+
+#ifdef __cplusplus
+	md_strb_t& operator += (str_t str);
+	md_strb_t& operator += (const char* cstr);
+	operator str_t() const;
+#endif
+
 } md_strb_t;
 
 #ifdef __cplusplus
@@ -17,6 +24,13 @@ extern "C" {
 
 void md_strb_init(md_strb_t* sb, struct md_allocator_i* alloc);
 void md_strb_free(md_strb_t* sb);
+
+static inline md_strb_t md_strb_create(struct md_allocator_i* alloc) {
+	ASSERT(alloc);
+	md_strb_t sb = {0};
+	md_strb_init(&sb, alloc);
+	return sb;
+}
 
 void md_strb_fmt(md_strb_t* sb, const char* format, ...);
 void md_strb_char(md_strb_t* sb, char c);
@@ -32,5 +46,20 @@ const char* md_strb_to_cstr(const md_strb_t* sb);
 str_t       md_strb_to_str(const md_strb_t* sb);
 
 #ifdef __cplusplus
+
+inline md_strb_t& md_strb_t::operator += (str_t str) {
+	md_strb_str(this, str);
+	return *this;
+}
+
+inline md_strb_t& md_strb_t::operator += (const char* cstr) {
+	md_strb_cstr(this, cstr);
+	return *this;
+}
+
+inline md_strb_t::operator str_t() const {
+	return md_strb_to_str(this);
+}
+
 }
 #endif
