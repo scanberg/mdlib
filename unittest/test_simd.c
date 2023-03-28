ef5a1b891b0a8e6acbc4de34f8600ae5886a784f
scanberg <scanberg@gmail.com>
2023-03-28 22:46:31 +0200

    simd: replaced movhdup instruction in hsum with shuffle to avoid SSE3 since only SSE2 is enabled by default on x64 simd_tests: added some more unittests

--- unittest/test_simd.c
+++ unittest/test_simd.c
@@ -747,10 +747,62 @@ static inline bool validate_closef(md_simd_f32_t x, float ref[], float eps) {
 	return true;
 }
 
-UTEST(simd, test) {
+UTEST(simd, sin_cos) {
 	md_simd_f32_t sin, cos;
     sincos256_ps(md_simd_set1_f32(1.0f), &sin, &cos);
 
 	EXPECT_TRUE(validate_close1f(sin, sinf(1.0f), 0.0001f));
 	EXPECT_TRUE(validate_close1f(cos, cosf(1.0f), 0.0001f));
 }
+
+UTEST(simd, hsum) {
+#if md_simd_f32_width >= 4
+    {
+    md_f32x4_t x = md_simd_set_f32x4(1.0f, 2.0f, 3.0f, 4.0f);
+    float sum = md_simd_hsum(x);
+    EXPECT_NEAR(sum, 10.0f, 0.0001f);
+    }
+#endif
+#if md_simd_f32_width >= 8
+    {
+    md_f32x8_t x = md_simd_set_f32x8(1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f);
+    float sum = md_simd_hsum(x);
+    EXPECT_NEAR(sum, 36.0f, 0.0001f);
+    }
+#endif
+}
+
+UTEST(simd, hmax) {
+#if md_simd_f32_width >= 4
+    {
+    md_f32x4_t x = md_simd_set_f32x4(1.0f, 2.0f, 3.0f, 4.0f);
+    float max = md_simd_hmax(x);
+    EXPECT_NEAR(max, 4.0f, 0.0001f);
+    }
+#endif
+#if md_simd_f32_width >= 8
+    {
+    md_f32x8_t x = md_simd_set_f32x8(1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f);
+    float max = md_simd_hmax(x);
+    EXPECT_NEAR(max, 8.0f, 0.0001f);
+    }
+#endif
+}
+
+UTEST(simd, hmin) {
+#if md_simd_f32_width >= 4
+    {
+    md_f32x4_t x = md_simd_set_f32x4(1.0f, 2.0f, -3.0f, 4.0f);
+    float min = md_simd_hmin(x);
+    EXPECT_NEAR(min, -3.0f, 0.0001f);
+    }
+#endif
+#if md_simd_f32_width >= 8
+    {
+    md_f32x8_t x = md_simd_set_f32x8(1.0f, 2.0f, 3.0f, -4.0f, 5.0f, 6.0f, 7.0f, 8.0f);
+    float min = md_simd_hmin(x);
+    EXPECT_NEAR(min, -4.0f, 0.0001f);
+    }
+#endif
+}
+
