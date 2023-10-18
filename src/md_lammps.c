6443be428782c6ae988b00b434d96a179b3bfbc1
Gustav Eriksson <guser40@liu.se>
2023-10-18 11:01:59 +0200

    Copies basic functionality for atom parsing from md_gro. Adds data_format for managing atom style variations

---
+++ src/md_lammps.c
@@ -0,0 +1,174 @@
+#include <md_lammps.h>
+
+#include <md_util.h>
+
+#include <core/md_common.h>
+#include <core/md_str.h>
+#include <core/md_allocator.h>
+#include <core/md_log.h>
+#include <core/md_os.h>
+#include <core/md_array.h>
+#include <core/md_parse.h>
+
+#include <string.h>
+
+static bool md_lammps_data_parse(md_lammps_data_t* data, md_buffered_reader_t* reader, struct md_allocator_i* alloc, data_format_t* data_format) {
+	ASSERT(data);
+	ASSERT(reader);
+	ASSERT(alloc);
+	str_t line;
+	str_t tokens[16];
+
+	//lammps_atom_data_structure test_data_structure[] = { ATOM_IDX, MOL_IDX, ATOM_TYPE, PARTIAL_CHARGE, ATOM_COORD, ATOM_COORD, ATOM_COORD };
+
+	//Read the title of the file
+	if (!md_buffered_reader_extract_line(&line, reader)) {
+		MD_LOG_ERROR("Failed to parse lammps title");
+		return false;
+	}
+	str_copy_to_char_buf(data->title, sizeof(data->title), str_trim(line));
+
+	//Skip empty line
+	md_buffered_reader_skip_line(reader);
+
+	//Read the number of atoms
+	if (!md_buffered_reader_extract_line(&line, reader)) {
+		MD_LOG_ERROR("Failed to parse LAMMPS num atoms");
+		return false;
+	}
+
+	data->num_atoms = parse_int(str_trim(line));
+	if (!data->num_atoms) {
+		MD_LOG_ERROR("Failed to parse LAMMPS num atoms");
+		return false;
+	}
+
+	md_array_resize(data->atom_data, data->num_atoms, alloc);
+
+	//Jump ahead to the Atoms definition
+	do {
+		md_buffered_reader_extract_line(&line, reader);
+	} while (!str_equal_cstr_n(line, "ATOMS", 5));
+
+	//Skip empty line
+	md_buffered_reader_skip_line(reader);
+
+	//Start reading the atom data
+	for (int64_t i = 0; i < data->num_atoms; ++i) {
+		if (!md_buffered_reader_extract_line(&line, reader)) {
+			MD_LOG_ERROR("Failed to extract atom line");
+			return false;
+		}
+		const int64_t num_tokens = extract_tokens(tokens, ARRAY_SIZE(tokens), &line);
+		if (num_tokens < data_format->len) {
+			MD_LOG_ERROR("Failed to parse atom coordinates, expected at least %i tokens, got %i", (int)data_format->len, (int)num_tokens);
+			return false;
+		}
+
+		md_lammps_atom_t* atom = &data->atom_data[i];
+
+		/*
+		ATOM_IDX,
+		MOL_IDX,
+		ATOM_TYPE,
+		PARTIAL_CHARGE,
+		ATOM_COORD
+		*/
+
+		for (int64_t d = 0; d < data_format->len; d++) {
+			switch (data_format->ptr[d])
+			{
+			default:
+				MD_LOG_ERROR("Failed to read atom data format");
+				return false;
+			case ATOM_IDX:
+				atom->atom_idx = (int32_t)parse_int_wide(tokens[d].ptr, tokens[d].len);
+				break;
+			case MOL_IDX:
+				atom->mol_idx = (int32_t)parse_int_wide(tokens[d].ptr, tokens[d].len);
+				break;
+			case ATOM_TYPE:
+				atom->atom_type = (int32_t)parse_int_wide(tokens[d].ptr, tokens[d].len);
+				break;
+			case PARTIAL_CHARGE:
+				atom->partial_charge = (float)parse_float_wide(tokens[d].ptr, tokens[d].len);
+				break;
+			case ATOM_X:
+				atom->x = (float)parse_float_wide(tokens[d].ptr, tokens[d].len);
+				break;
+			case ATOM_Y:
+				atom->y = (float)parse_float_wide(tokens[d].ptr, tokens[d].len);
+				break;
+			case ATOM_Z:
+				atom->z = (float)parse_float_wide(tokens[d].ptr, tokens[d].len);
+				break;
+			case NX:
+				atom->atom_type = (int32_t)parse_int_wide(tokens[d].ptr, tokens[d].len);
+				break;
+			case NY:
+				atom->atom_type = (int32_t)parse_int_wide(tokens[d].ptr, tokens[d].len);
+				break;
+			case NZ:
+				atom->atom_type = (int32_t)parse_int_wide(tokens[d].ptr, tokens[d].len);
+				break;
+			}
+		}
+
+		//str_copy_to_char_buf(atom->atom_type, sizeof(atom->atom_type), str_trim(str_substr(line, 5, 5)));
+		//str_copy_to_char_buf(atom->atom_name, sizeof(atom->atom_name), str_trim(str_substr(line, 10, 5)));
+	}
+	/*
+
+	if (!md_buffered_reader_extract_line(&line, reader)) {
+		MD_LOG_ERROR("Failed to extract unitcell line");
+		return false;
+	}
+
+	const int64_t num_tokens = extract_float_tokens(tokens, ARRAY_SIZE(tokens), line);
+
+	if (num_tokens != 3) {
+		MD_LOG_ERROR("Failed to parse cell extent, expected 3 tokens, got %i", (int)num_tokens);
+		return false;
+	}
+
+	data->cell_ext[0] = (float)parse_float(tokens[0]);
+	data->cell_ext[1] = (float)parse_float(tokens[1]);
+	data->cell_ext[2] = (float)parse_float(tokens[2]);
+
+	*/
+
+	return true;
+}
+
+bool md_lammps_data_parse_str(md_lammps_data_t* data, str_t str, struct md_allocator_i* alloc, data_format_t* data_format) {
+	ASSERT(data);
+	ASSERT(alloc);
+
+	md_buffered_reader_t line_reader = md_buffered_reader_from_str(str);
+	return md_lammps_data_parse(data, &line_reader, alloc, data_format);
+}
+
+bool md_lammps_data_parse_file(md_lammps_data_t* data, str_t filename, struct md_allocator_i* alloc, data_format_t* data_format) {
+	bool result = false;
+	md_file_o* file = md_file_open(filename, MD_FILE_READ | MD_FILE_BINARY);
+	if (file) {
+		const int64_t cap = MEGABYTES(1);
+		char* buf = md_alloc(md_heap_allocator, cap);
+
+		md_buffered_reader_t line_reader = md_buffered_reader_from_file(buf, cap, file);
+		result = md_lammps_data_parse(data, &line_reader, alloc, data_format);
+
+		md_free(md_heap_allocator, buf, cap);
+		md_file_close(file);
+	}
+	else {
+		MD_LOG_ERROR("Could not open file '%.*s'", filename.len, filename.ptr);
+	}
+	return result;
+}
+
+void md_lammps_data_free(md_lammps_data_t* data, struct md_allocator_i* alloc) {
+	ASSERT(data);
+	if (data->atom_data) md_array_free(data->atom_data, alloc);
+	MEMSET(data, 0, sizeof(md_lammps_data_t));
+}
---
+++ src/md_lammps.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <core/md_str.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct md_allocator_i;
+struct md_molecule_t;
+struct md_molecule_loader_i;
+
+typedef enum lammps_atom_data_format {
+	ATOM_IDX,
+	MOL_IDX,
+	ATOM_TYPE,
+	PARTIAL_CHARGE,
+	ATOM_X,
+	ATOM_Y,
+	ATOM_Z,
+	NX,
+	NY,
+	NZ
+} lammps_atom_data_format;
+
+typedef struct data_format_t {
+	const lammps_atom_data_format* ptr;
+	int64_t len;
+} data_format_t;
+
+typedef struct md_lammps_atom_t {
+	int32_t atom_idx;
+	int32_t mol_idx;
+	int32_t atom_type;
+	float partial_charge;
+	float x;
+	float y;
+	float z;
+} md_lammps_atom_t;
+
+typedef struct md_lammps_atom_mass_t {
+	int32_t atom_idx;
+	float mass;
+} md_lammps_atom_mass_t;
+
+typedef struct md_lammps_data_t {
+	char title[256];
+	float cell_ext[3];
+
+	md_lammps_atom_mass_t* atom_type_mass;
+
+	// Field data
+	int64_t num_atoms;
+	md_lammps_atom_t* atom_data;
+} md_lammps_data_t;
+
+// Parse a text-blob as LAMMPS
+bool md_lammps_data_parse_str(md_lammps_data_t* data, str_t str, struct md_allocator_i* alloc, data_format_t* data_format);
+bool md_lammps_data_parse_file(md_lammps_data_t* data, str_t filename, struct md_allocator_i* alloc, data_format_t* data_format);
+void md_lammps_data_free(md_lammps_data_t* data, struct md_allocator_i* alloc);
+
+// Molecule
+bool md_lammps_molecule_init(struct md_molecule_t* mol, const md_lammps_data_t* lammps_data, struct md_allocator_i* alloc);
+
+struct md_molecule_loader_i* md_lammps_molecule_api();
+
+#ifdef __cplusplus
+}
+#endif
+
