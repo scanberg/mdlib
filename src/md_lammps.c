5f236e55062403c7236f44c5fbc20de804a9c9cc
Gustav Eriksson <guser40@liu.se>
2023-10-30 10:50:40 +0100

    Contains code within blocks

--- src/md_lammps.c
+++ src/md_lammps.c
@@ -35,252 +35,283 @@ static bool md_lammps_data_parse(md_lammps_data_t* data, md_buffered_reader_t* r
 	//lammps_atom_data_structure test_data_structure[] = { ATOM_IDX, MOL_IDX, ATOM_TYPE, PARTIAL_CHARGE, ATOM_COORD, ATOM_COORD, ATOM_COORD };
 
 	//Read the title of the file
-	if (!md_buffered_reader_extract_line(&line, reader)) {
-		MD_LOG_ERROR("Failed to parse lammps title");
-		return false;
-	}
-	str_copy_to_char_buf(data->title, sizeof(data->title), str_trim(line));
+	{
+		if (!md_buffered_reader_extract_line(&line, reader)) {
+			MD_LOG_ERROR("Failed to parse lammps title");
+			return false;
+		}
+		str_copy_to_char_buf(data->title, sizeof(data->title), str_trim(line));
 
-	//Skip empty line
-	md_buffered_reader_skip_line(reader);
+		//Skip empty line
+		md_buffered_reader_skip_line(reader);
+	}
 
 	//Read the number of atoms
-	if (!md_buffered_reader_extract_line(&line, reader)) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num atoms");
-		return false;
-	}
-	data->num_atoms = parse_int(str_trim(line));
-	if (!data->num_atoms) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num atoms");
-		return false;
-	}
+	{
+		if (!md_buffered_reader_extract_line(&line, reader)) {
+			MD_LOG_ERROR("Failed to parse LAMMPS num atoms");
+			return false;
+		}
+		data->num_atoms = parse_int(str_trim(line));
+		if (!data->num_atoms) {
+			MD_LOG_ERROR("Failed to parse LAMMPS num atoms");
+			return false;
+		}
 
-	md_array_resize(data->atom_data, data->num_atoms, alloc);
+		md_array_resize(data->atom_data, data->num_atoms, alloc);
+	}
 
 	//Read num atom types
-	if (!md_buffered_reader_extract_line(&line, reader)) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num atom types");
-		return false;
+	{
+		if (!md_buffered_reader_extract_line(&line, reader)) {
+			MD_LOG_ERROR("Failed to parse LAMMPS num atom types");
+			return false;
+		}
+		data->num_atom_types = (int32_t)parse_int(str_trim(line));
+		if (!data->num_atom_types) {
+			MD_LOG_ERROR("Failed to parse LAMMPS num atom types");
+			return false;
+		}
+		md_array_resize(data->atom_type_mass, data->num_atom_types, alloc);
 	}
-	data->num_atom_types = parse_int(str_trim(line));
-	if (!data->num_atom_types) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num atom types");
-		return false;
-	}
-
-	md_array_resize(data->atom_type_mass, data->num_atom_types, alloc);
 
 	//Read num bonds
-	if (!md_buffered_reader_extract_line(&line, reader)) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num bonds");
-		return false;
-	}
-	data->num_bonds = parse_int(str_trim(line));
-	if (!data->num_bonds) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num bonds");
-		return false;
-	}
+	{
+		if (!md_buffered_reader_extract_line(&line, reader)) {
+			MD_LOG_ERROR("Failed to parse LAMMPS num bonds");
+			return false;
+		}
+		data->num_bonds = parse_int(str_trim(line));
+		if (!data->num_bonds) {
+			MD_LOG_ERROR("Failed to parse LAMMPS num bonds");
+			return false;
+		}
 
-	md_array_resize(data->bonds, data->num_bonds, alloc);
+		md_array_resize(data->bonds, data->num_bonds, alloc);
+	}
 
 	//Read num bond types
-	if (!md_buffered_reader_extract_line(&line, reader)) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num bond types");
-		return false;
-	}
-	data->num_bond_types = parse_int(str_trim(line));
-	if (!data->num_bond_types) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num bond types");
-		return false;
+	{
+		if (!md_buffered_reader_extract_line(&line, reader)) {
+			MD_LOG_ERROR("Failed to parse LAMMPS num bond types");
+			return false;
+		}
+		data->num_bond_types = (int32_t)parse_int(str_trim(line));
+		if (!data->num_bond_types) {
+			MD_LOG_ERROR("Failed to parse LAMMPS num bond types");
+			return false;
+		}
 	}
 
 	//Read num angles
-	if (!md_buffered_reader_extract_line(&line, reader)) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num angles");
-		return false;
-	}
-	data->num_angles = parse_int(str_trim(line));
-	if (!data->num_angles) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num angles");
-		return false;
+	{
+		if (!md_buffered_reader_extract_line(&line, reader)) {
+			MD_LOG_ERROR("Failed to parse LAMMPS num angles");
+			return false;
+		}
+		data->num_angles = parse_int(str_trim(line));
+		if (!data->num_angles) {
+			MD_LOG_ERROR("Failed to parse LAMMPS num angles");
+			return false;
+		}
 	}
 
 	//Read num angle types
-	if (!md_buffered_reader_extract_line(&line, reader)) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num angle types");
-		return false;
-	}
-	data->num_angle_types = parse_int(str_trim(line));
-	if (!data->num_angle_types) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num angle types");
-		return false;
+	{
+		if (!md_buffered_reader_extract_line(&line, reader)) {
+			MD_LOG_ERROR("Failed to parse LAMMPS num angle types");
+			return false;
+		}
+		data->num_angle_types = (int32_t)parse_int(str_trim(line));
+		if (!data->num_angle_types) {
+			MD_LOG_ERROR("Failed to parse LAMMPS num angle types");
+			return false;
+		}
 	}
 
 	//Read num dihedrals
-	if (!md_buffered_reader_extract_line(&line, reader)) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num dihedrals");
-		return false;
-	}
-	data->num_dihedrals = parse_int(str_trim(line));
-	if (!data->num_dihedrals) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num dihedrals");
-		return false;
+	{
+		if (!md_buffered_reader_extract_line(&line, reader)) {
+			MD_LOG_ERROR("Failed to parse LAMMPS num dihedrals");
+			return false;
+		}
+		data->num_dihedrals = parse_int(str_trim(line));
+		if (!data->num_dihedrals) {
+			MD_LOG_ERROR("Failed to parse LAMMPS num dihedrals");
+			return false;
+		}
 	}
 
 	//Read num dihedral types
-	if (!md_buffered_reader_extract_line(&line, reader)) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num dihedral types");
-		return false;
-	}
-	data->num_dihedral_types = parse_int(str_trim(line));
-	if (!data->num_dihedral_types) {
-		MD_LOG_ERROR("Failed to parse LAMMPS num dihedral types");
-		return false;
-	}
-
-	//Jump ahead to the Masses definition
-	do {
-		
+	{
 		if (!md_buffered_reader_extract_line(&line, reader)) {
-			MD_LOG_ERROR("Could not find Masses line");
+			MD_LOG_ERROR("Failed to parse LAMMPS num dihedral types");
 			return false;
 		}
-	} while (!str_equal_cstr_n(line, "Masses", 6));
-
-	//Skip empty line
-	md_buffered_reader_skip_line(reader);
-
-	//Start reading the Masses data
-
-	for (int32_t i = 0; i < data->num_atom_types; i++) {
-		if (!md_buffered_reader_extract_line(&line, reader)) {
-			MD_LOG_ERROR("Failed to extract mass line");
+		data->num_dihedral_types = (int32_t)parse_int(str_trim(line));
+		if (!data->num_dihedral_types) {
+			MD_LOG_ERROR("Failed to parse LAMMPS num dihedral types");
 			return false;
 		}
-		if (extract_tokens(tokens, 2, &line) != 2) {
-			MD_LOG_ERROR("Wrong amount of mass tokens");
-			return false;
-		}
-
-		md_lammps_atom_mass_t* mass = &data->atom_type_mass[i];
-
-		mass->atom_idx = (int32_t)parse_int(tokens[0]);
-		/*
-		if (!data->atom_type_mass->atom_idx) {
-			MD_LOG_ERROR("Atom mass index was not written to data");
-			return false;
-		}
-		*/
-		mass->mass = (float)parse_float(tokens[1]);
 	}
 
-	//Jump ahead to the Atoms definition
-	do {
-		
-		if (!md_buffered_reader_extract_line(&line, reader)) {
-			MD_LOG_ERROR("Could not find Atoms line");
-			return false;
-		}
-	} while (!str_equal_cstr_n(line, "Atoms", 5));
+	//Mass parsing
+	{
+		//Jump ahead to the Masses definition
+		{
+			do {
 
-	//Skip empty line
-	md_buffered_reader_skip_line(reader);
+				if (!md_buffered_reader_extract_line(&line, reader)) {
+					MD_LOG_ERROR("Could not find Masses line");
+					return false;
+				}
+			} while (!str_equal_cstr_n(line, "Masses", 6));
 
-	//Start reading the atom data
-	for (int64_t i = 0; i < data->num_atoms; ++i) {
-		if (!md_buffered_reader_extract_line(&line, reader)) {
-			MD_LOG_ERROR("Failed to extract atom line");
-			return false;
-		}
-		const int64_t num_tokens = extract_tokens(tokens, ARRAY_SIZE(tokens), &line);
-		if (num_tokens < data_format->len) {
-			MD_LOG_ERROR("Failed to parse atom coordinates, expected at least %i tokens, got %i", (int)data_format->len, (int)num_tokens);
-			return false;
+			//Skip empty line
+			md_buffered_reader_skip_line(reader);
 		}
 
-		md_lammps_atom_t* atom = &data->atom_data[i];
-
-		/*
-		ATOM_IDX,
-		MOL_IDX,
-		ATOM_TYPE,
-		PARTIAL_CHARGE,
-		ATOM_COORD
-		*/
-
-		for (int64_t d = 0; d < data_format->len; d++) {
-			switch (data_format->ptr[d])
-			{
-			default:
-				MD_LOG_ERROR("Failed to read atom data format");
+		//Start reading the Masses data
+		for (int32_t i = 0; i < data->num_atom_types; i++) {
+			if (!md_buffered_reader_extract_line(&line, reader)) {
+				MD_LOG_ERROR("Failed to extract mass line");
 				return false;
-			case ATOM_IDX:
-				atom->atom_idx = (int32_t)parse_int(tokens[d]);
-				break;
-			case MOL_IDX:
-				atom->mol_idx = (int32_t)parse_int(tokens[d]);
-				break;
-			case ATOM_TYPE:
-				atom->atom_type = (int32_t)parse_int(tokens[d]);
-				break;
-			case PARTIAL_CHARGE:
-				atom->partial_charge = (float)parse_float(tokens[d]);
-				break;
-			case ATOM_X:
-				atom->x = (float)parse_float(tokens[d]);
-				break;
-			case ATOM_Y:
-				atom->y = (float)parse_float(tokens[d]);
-				break;
-			case ATOM_Z:
-				atom->z = (float)parse_float(tokens[d]);
-				break;
-			case NX:
-				atom->nx = (int32_t)parse_int(tokens[d]);
-				break;
-			case NY:
-				atom->ny = (int32_t)parse_int(tokens[d]);
-				break;
-			case NZ:
-				atom->nz = (int32_t)parse_int(tokens[d]);
-				break;
-			case res_name:
-				str_copy_to_char_buf(atom->res_name, sizeof(atom->res_name), tokens[d]);
 			}
-		}
+			if (extract_tokens(tokens, 2, &line) != 2) {
+				MD_LOG_ERROR("Wrong amount of mass tokens");
+				return false;
+			}
 
-		//str_copy_to_char_buf(atom->atom_type, sizeof(atom->atom_type), str_trim(str_substr(line, 5, 5)));
-		//str_copy_to_char_buf(atom->atom_name, sizeof(atom->atom_name), str_trim(str_substr(line, 10, 5)));
+			md_lammps_atom_mass_t* mass = &data->atom_type_mass[i];
+
+			mass->atom_idx = (int32_t)parse_int(tokens[0]);
+			/*
+			if (!data->atom_type_mass->atom_idx) {
+				MD_LOG_ERROR("Atom mass index was not written to data");
+				return false;
+			}
+			*/
+			mass->mass = (float)parse_float(tokens[1]);
+		}
 	}
 
-	//Jump ahead to the Bonds definition
-	do {
-		
-		if (!md_buffered_reader_extract_line(&line, reader)) {
-			MD_LOG_ERROR("Could not find Bonds line");
-			return false;
-		}
-	} while (!str_equal_cstr_n(line, "Bonds", 5));
-	//Skip empty line
-	md_buffered_reader_skip_line(reader);
+	//Atom parsing
+	{
+		//Jump ahead to the Atoms definition
+		{
+			do {
 
-	//Start reading the bonds
-	for (int32_t i = 0; i < data->num_bonds; i++) {
-		if (!md_buffered_reader_extract_line(&line, reader)) {
-			MD_LOG_ERROR("Failed to extract bond line");
-			return false;
-		}
-		if (extract_tokens(tokens, 4, &line) != 4) {
-			MD_LOG_ERROR("Wrong amount of bond tokens");
-			return false;
+				if (!md_buffered_reader_extract_line(&line, reader)) {
+					MD_LOG_ERROR("Could not find Atoms line");
+					return false;
+				}
+			} while (!str_equal_cstr_n(line, "Atoms", 5));
+
+			//Skip empty line
+			md_buffered_reader_skip_line(reader);
 		}
 
-		md_lammps_atom_bond_t* bond = &data->bonds[i];
+		//Start reading the atom data
+		for (int64_t i = 0; i < data->num_atoms; ++i) {
+			if (!md_buffered_reader_extract_line(&line, reader)) {
+				MD_LOG_ERROR("Failed to extract atom line");
+				return false;
+			}
+			const int64_t num_tokens = extract_tokens(tokens, ARRAY_SIZE(tokens), &line);
+			if (num_tokens < data_format->len) {
+				MD_LOG_ERROR("Failed to parse atom coordinates, expected at least %i tokens, got %i", (int)data_format->len, (int)num_tokens);
+				return false;
+			}
 
-		bond->bond_idx = (int64_t)parse_int(tokens[0]);
-		bond->bond_type = (int32_t)parse_int(tokens[1]);
-		bond->first_atom_idx = (int64_t)parse_int(tokens[2]);
-		bond->second_atom_idx = (int64_t)parse_int(tokens[3]);
+			md_lammps_atom_t* atom = &data->atom_data[i];
+
+			/*
+			ATOM_IDX,
+			MOL_IDX,
+			ATOM_TYPE,
+			PARTIAL_CHARGE,
+			ATOM_COORD
+			*/
+
+			for (int64_t d = 0; d < data_format->len; d++) {
+				switch (data_format->ptr[d])
+				{
+				default:
+					MD_LOG_ERROR("Failed to read atom data format");
+					return false;
+				case ATOM_IDX:
+					atom->atom_idx = (int32_t)parse_int(tokens[d]);
+					break;
+				case MOL_IDX:
+					atom->mol_idx = (int32_t)parse_int(tokens[d]);
+					break;
+				case ATOM_TYPE:
+					atom->atom_type = (int32_t)parse_int(tokens[d]);
+					break;
+				case PARTIAL_CHARGE:
+					atom->partial_charge = (float)parse_float(tokens[d]);
+					break;
+				case ATOM_X:
+					atom->x = (float)parse_float(tokens[d]);
+					break;
+				case ATOM_Y:
+					atom->y = (float)parse_float(tokens[d]);
+					break;
+				case ATOM_Z:
+					atom->z = (float)parse_float(tokens[d]);
+					break;
+				case NX:
+					atom->nx = (int32_t)parse_int(tokens[d]);
+					break;
+				case NY:
+					atom->ny = (int32_t)parse_int(tokens[d]);
+					break;
+				case NZ:
+					atom->nz = (int32_t)parse_int(tokens[d]);
+					break;
+				case res_name:
+					str_copy_to_char_buf(atom->res_name, sizeof(atom->res_name), tokens[d]);
+				}
+			}
+
+			//str_copy_to_char_buf(atom->atom_type, sizeof(atom->atom_type), str_trim(str_substr(line, 5, 5)));
+			//str_copy_to_char_buf(atom->atom_name, sizeof(atom->atom_name), str_trim(str_substr(line, 10, 5)));
+		}
+	}
+
+	//Bonds parsing
+	{
+		//Jump ahead to the Bonds definition
+		{
+			do {
+
+				if (!md_buffered_reader_extract_line(&line, reader)) {
+					MD_LOG_ERROR("Could not find Bonds line");
+					return false;
+				}
+			} while (!str_equal_cstr_n(line, "Bonds", 5));
+			//Skip empty line
+			md_buffered_reader_skip_line(reader);
+		}
+
+		//Start reading the bonds
+		for (int32_t i = 0; i < data->num_bonds; i++) {
+			if (!md_buffered_reader_extract_line(&line, reader)) {
+				MD_LOG_ERROR("Failed to extract bond line");
+				return false;
+			}
+			if (extract_tokens(tokens, 4, &line) != 4) {
+				MD_LOG_ERROR("Wrong amount of bond tokens");
+				return false;
+			}
+
+			md_lammps_atom_bond_t* bond = &data->bonds[i];
+
+			bond->bond_idx = (int64_t)parse_int(tokens[0]);
+			bond->bond_type = (int32_t)parse_int(tokens[1]);
+			bond->first_atom_idx = (int64_t)parse_int(tokens[2]);
+			bond->second_atom_idx = (int64_t)parse_int(tokens[3]);
+		}
 	}
 
 	/*
