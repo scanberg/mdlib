1957c9b8324302194db9fcced487bb43490ad292
scanberg <scanberg@gmail.com>
2024-02-12 10:24:43 +0100

    fixed warning

--- src/md_xtc.c
+++ src/md_xtc.c
@@ -314,7 +314,7 @@ static bool xtc_decode_frame_data(struct md_trajectory_o* inst, const void* fram
             result = xtc_frame_coords(file, natoms, pos);
             if (result) {            
                 // nm -> Ångström
-                for (size_t i = 0; i < natoms; ++i) {
+                for (int i = 0; i < natoms; ++i) {
                     if (x) x[i] = pos[i][0] * 10.0f;
                     if (y) y[i] = pos[i][1] * 10.0f;
                     if (z) z[i] = pos[i][2] * 10.0f;
