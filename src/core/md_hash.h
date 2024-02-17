8c804fd6c887417467de6f1cf2ea6d652a99e3eb
scanberg <scanberg@gmail.com>
2024-02-18 00:35:24 +0100

    hash: _H1 had symbol collision with something defined in some std header. And it was not correct.

--- src/core/md_hash.h
+++ src/core/md_hash.h
@@ -5,13 +5,13 @@
 
 // https://stackoverflow.com/questions/2826559/compile-time-preprocessor-hashing-of-string
 // http://lolengine.net/blog/2011/12/20/cpp-constant-string-hash
-#define _H1(s,i,x)   (x*65599u+(uint8_t)s[(i)<sizeof(s)?sizeof(s)-1-(i):sizeof(s)])
-#define _H4(s,i,x)   H1(s,i,H1(s,i+1,H1(s,i+2,H1(s,i+3,x))))
-#define _H16(s,i,x)  H4(s,i,H4(s,i+4,H4(s,i+8,H4(s,i+12,x))))
-#define _H64(s,i,x)  H16(s,i,H16(s,i+16,H16(s,i+32,H16(s,i+48,x))))
-#define _H256(s,i,x) H64(s,i,H64(s,i+64,H64(s,i+128,H64(s,i+192,x))))
+#define HASH_H1(s,i,x)   (x*65599u+(uint8_t)s[(i)<sizeof(s)?sizeof(s)-1-(i):sizeof(s)])
+#define HASH_H4(s,i,x)   HASH_H1 (s,i,HASH_H1 (s,i+1, HASH_H1 (s,i+2,  HASH_H1 (s,i+3,x))))
+#define HASH_H16(s,i,x)  HASH_H4 (s,i,HASH_H4 (s,i+4, HASH_H4 (s,i+8,  HASH_H4 (s,i+12,x))))
+#define HASH_H64(s,i,x)  HASH_H16(s,i,HASH_H16(s,i+16,HASH_H16(s,i+32, HASH_H16(s,i+48,x))))
+#define HASH_H256(s,i,x) HASH_H64(s,i,HASH_H64(s,i+64,HASH_H64(s,i+128,HASH_H64(s,i+192,x))))
 // Hash a string literal (at compile time)
-#define HASH_STR_LIT(s)  ((uint32_t)(H256(s"",0,0)^(H256(s"",0,0)>>16)))
+#define HASH_STR_LIT(s)  ((uint32_t)(HASH_H256(s"",0,0)^(HASH_H256(s"",0,0)>>16)))
 
 #ifdef __cplusplus
 extern "C" {
