c50b4c01b635481393432819268fc9e3b4d07852
scanberg <scanberg@gmail.com>
2025-08-15 14:28:11 +0200

    small changes

--- src/md_molecule.h
+++ src/md_molecule.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -262,24 +262,24 @@ static inline uint32_t md_bond_iter_bond_flags(md_bond_iter_t it) {
 
 static inline void md_bond_conn_clear(md_conn_data_t* conn_data) {
     ASSERT(conn_data);
+    conn_data->count = 0;
     md_array_shrink(conn_data->atom_idx, 0);
     md_array_shrink(conn_data->bond_idx, 0);
-    conn_data->count = 0;
 
-    md_array_shrink(conn_data->offset, 0);
     conn_data->offset_count = 0;
+    md_array_shrink(conn_data->offset, 0);
 }
 
 static inline void md_bond_data_clear(md_bond_data_t* bond_data) {
     ASSERT(bond_data);
 
+    bond_data->count = 0;
     md_array_shrink(bond_data->pairs, 0);
     md_array_shrink(bond_data->order, 0);
-    bond_data->count = 0;
     
+    bond_data->conn.count = 0;
     md_array_shrink(bond_data->conn.atom_idx, 0);
     md_array_shrink(bond_data->conn.bond_idx, 0);
-    bond_data->conn.count = 0;
 
     md_bond_conn_clear(&bond_data->conn);
 }
