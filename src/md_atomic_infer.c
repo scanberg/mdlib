4d2fa26395ce1604839c7263c30b89686a2a6110
Robin Skånberg <robsk@kth.se>
2025-09-29 12:27:39 +0200

    Added predefined table to resolve some atom types missed by the ineference scheme.

--- src/md_atomic_infer.c
+++ src/md_atomic_infer.c
@@ -43,6 +43,64 @@ uint32_t md_atomic_number_cpk_color(md_atomic_number_t z) {
     return md_util_element_cpk_color(z);
 }
 
+typedef struct {
+	str_t str;
+    md_atomic_number_t z;
+} Mapping;
+
+#define STR(str) {str, sizeof(str)-1}
+
+// Predefined mappings (applies to a combination from res_name:atom_name)
+static const Mapping mappings[] = {
+	// Common ions
+	{STR("NA:NA"), MD_Z_Na},
+	{STR("SOD:SOD"), MD_Z_Na},
+	{STR("SOD:NA"),MD_Z_Na},
+	{STR("K:K"),   MD_Z_K},
+	{STR("POT:K"), MD_Z_K},
+	{STR("CL:CL"), MD_Z_Cl},
+	{STR("CLA:CL"),MD_Z_Cl},
+    {STR("CLA:CLA"),MD_Z_Cl},
+	{STR("CA:CA"), MD_Z_Ca},
+	{STR("CAL:CA"),MD_Z_Ca},
+	{STR("MG:MG"), MD_Z_Mg},
+	{STR("MAG:MG"),MD_Z_Mg},
+	{STR("ZN:ZN"), MD_Z_Zn},
+	{STR("FE:FE"), MD_Z_Fe},
+	{STR("CU:CU"), MD_Z_Cu},
+	{STR("MN:MN"), MD_Z_Mn},
+	{STR("F: F"),  MD_Z_F},
+	{STR("BR:BR"), MD_Z_Br},
+	{STR("I: I"),  MD_Z_I},
+	{STR("IOD:I"), MD_Z_I},
+
+    // Other
+    {STR("POPC:HS"), MD_Z_H},
+    {STR("HOH:O"), MD_Z_O},
+    {STR("WAT:O"), MD_Z_O},
+    {STR("H2O:O"), MD_Z_O},
+    {STR("DOD:O"), MD_Z_O}, // Heavy water
+    {STR("CO3:C"), MD_Z_C}, // Carbonate
+    {STR("CO3:O"), MD_Z_O},
+    {STR("CO2:C"), MD_Z_C}, // Carbon dioxide
+    {STR("CO2:O"), MD_Z_O},
+    {STR("NH4:N"), MD_Z_N}, // Ammonium
+    {STR("NH4:H"), MD_Z_H},
+    {STR("NO3:N"), MD_Z_N}, // Nitrate
+    {STR("NO3:O"), MD_Z_O},
+    {STR("SO4:S"), MD_Z_S}, // Sulfate
+    {STR("SO4:O"), MD_Z_O},
+};
+
+static inline md_atomic_number_t find_in_mappings(str_t key, const Mapping* mappings, size_t num_mappings) {
+    for (size_t i = 0; i < num_mappings; ++i) {
+        if (str_eq_ignore_case(key, mappings[i].str)) {
+            return mappings[i].z;
+        }
+    }
+    return MD_Z_X;
+}
+
 // Inference functions
 md_atomic_number_t md_atomic_number_infer_from_label(str_t atom_name, str_t res_name) {    
 
@@ -65,12 +123,20 @@ md_atomic_number_t md_atomic_number_infer_from_label(str_t atom_name, str_t res_
             md_atomic_number_t z = md_atomic_number_from_symbol(first_char, true);
             if (z != MD_Z_X) return z;
         }
+
+		char combined_key_buf[16];
+		int len = snprintf(combined_key_buf, sizeof(combined_key_buf), STR_FMT ":" STR_FMT, STR_ARG(res_name), STR_ARG(atom_name));
+		str_t combined_key = { combined_key_buf, (size_t)len };
+
+		// Check predefined mappings first
+        md_atomic_number_t z = find_in_mappings(combined_key, mappings, ARRAY_SIZE(mappings));
+        if (z) return z;
         
-        // If residue name itself is an element (ion case)
+        // If residue name itself is an element (ions for example)
         md_atomic_number_t res_element = md_atomic_number_from_symbol(res_name, true);
         if (res_element != MD_Z_X) {
             // If atom name is empty or equals residue, return that element
-            if (atom_name.len == 0 || str_eq_ignore_case(atom_name, res_name)) {
+            if (str_eq_ignore_case(atom_name, res_name)) {
                 return res_element;
             }
         }
