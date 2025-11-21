f356c95b9005310501b55519d4afa90021626049
scanberg <scanberg@gmail.com>
2025-11-21 11:22:05 +0100

    cmake fixes

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,10 +1,10 @@
 ﻿# We do not want to have a hard dependency on VCPKG
 # As it is only used for big external libs
 # So if VCPKG_ROOT is defined in the environment, we use the cmake toolchain of vcpkg
-if (NOT DEFINED {CMAKE_TOOLCHAIN_FILE} AND DEFINED ENV{VCPKG_ROOT})
-    set(VCPKG_CRT_LINKAGE static)
-    set(VCPKG_LIBRARY_LINKAGE static)
-    set(VCPKG_TARGET_ARCHITECTURE x64)
+if (NOT DEFINED CMAKE_TOOLCHAIN_FILE AND DEFINED ENV{VCPKG_ROOT})
+    #set(VCPKG_CRT_LINKAGE static)
+    #set(VCPKG_LIBRARY_LINKAGE static)
+    #set(VCPKG_TARGET_ARCHITECTURE x64)
 
     if(WIN32)
         set(VCPKG_TARGET_TRIPLET "x64-windows-static" CACHE STRING "VCPKG Target Triplet to use")
